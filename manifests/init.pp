b579906b28b7825f97b13783535b52b2e3de26c1
Matthew Hope <mhope@adaptavist.com>
2015-12-29 22:17:38 +0000

    lint fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,9 @@ class xvfb(
     RedHat: {
       $package = 'xorg-x11-server-Xvfb'
     }
+    default: {
+      fail("xvfb - Unsupported Operating System family: ${::osfamily}")
+    }
   }
 
   unless defined(Package[$package]) {
@@ -19,8 +22,8 @@ class xvfb(
   }
 
   file { '/etc/init.d/xvfb':
-    content => template("$module_name/xvfb.erb"),
-    mode    => 755,
+    content => template("${module_name}/xvfb.erb"),
+    mode    => '0755',
     require => Package[$package]
   }
 
--- spec/classes/xvfb_spec.rb
+++ spec/classes/xvfb_spec.rb
@@ -11,7 +11,7 @@ describe 'xvfb', :type => 'class' do
       should contain_package('xvfb')
       should contain_file('/etc/init.d/xvfb')
         .with(
-          'mode' => 755
+          'mode' => '0755'
         )
         .with_content(/USER=hosting/)
         .with_content(/XVFBARGS=":99 -ac"/)
@@ -29,11 +29,21 @@ describe 'xvfb', :type => 'class' do
       should contain_package('xvfb')
       should contain_file('/etc/init.d/xvfb')
         .with(
-          'mode' => 755
+          'mode' => '0755'
         )
         .with_content(/USER=spec/)
         .with_content(/XVFBARGS=":20 -once"/)
       should contain_service('xvfb')
+    end
+  end
+
+  context "Should fail with unsupported OS family" do
+    let(:facts) {
+     { :osfamily     => 'Solaris' }
+    }
+
+    it do
+      should raise_error(Puppet::Error, /xvfb - Unsupported Operating System family: Solaris/)
     end
   end
 end
